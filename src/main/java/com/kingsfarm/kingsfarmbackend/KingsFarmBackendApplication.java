468b859951a71b19933785c4d78da96ba4a6e896
Oluwafemijohn1 <femiogundipe01@gmail.com>
2026-08-28 23:36:15 +0100

    Initial project

---
+++ src/main/java/com/kingsfarm/kingsfarmbackend/KingsFarmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kingsfarm.kingsfarmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingsFarmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingsFarmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kingsfarm/kingsfarmbackend/KingsFarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingsfarm.kingsfarmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingsFarmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
